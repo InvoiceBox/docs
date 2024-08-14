91dd53fd07846324a25a5eb77d4d15e4f8cb0e48
Grigoriy Borozdin <grigory.borozdin@invbox.ru>
2024-08-14 17:55:00 +0300

    Создание локальной сборки

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
 #docker run --rm -p 4000:4000 -v /$PWD:/site bretfisher/jekyll-serve
 #gem install eventmachine --platform ruby
-bundle exec jekyll serve --livereload
+bundle install
+bundle exec jekyll serve --host localhost --livereload
